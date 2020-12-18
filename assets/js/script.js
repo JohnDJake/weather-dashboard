35b7f3183e9e490fbc4398839767eabe63dbaa1e
JohnDJake <johndjake@gmail.com>
2020-12-17 16:56:49 -0800

    added the current date to the title that displays the city

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,15 @@ function updateWeather(city) {
         url: `https://api.opencagedata.com/geocode/v1/json?key=92f5df1355c8480eabe387e3cb8b8bbf&q=${city}&limit=1&no_annotations=1`,
         method: "GET"
     }).then(function (coordResponse) {
-        // Set the title based off the location info from OpenCage
-        $("#title-today").text(`${coordResponse.results[0].components.city}, ${coordResponse.results[0].components.state_code},  ${coordResponse.results[0].components["ISO_3166-1_alpha-3"]}`);
+        const coordResult = coordResponse.results[0];
+        console.log(coordResult);
         // Get the weather data using the coordinates from OpenCage
         $.ajax({
-            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${coordResponse.results[0].geometry.lat}&lon=${coordResponse.results[0].geometry.lng}&exclude=minutely,hourly,alerts&appid=d299da3abaf6094099f1ec02d54a1339`,
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${coordResult.geometry.lat}&lon=${coordResult.geometry.lng}&exclude=minutely,hourly,alerts&appid=d299da3abaf6094099f1ec02d54a1339`,
             method: "GET"
         }).then(function (weatherResponse) {
+            // Set the title based off the location info from OpenCage and the date from OpenWeather
+            $("#title-today").text(`${coordResult.components.city}, ${coordResult.components.state_code},  ${coordResult.components["ISO_3166-1_alpha-3"]} (${DateTime.fromSeconds(weatherResponse.current.dt).toLocaleString()})`);
             console.log(weatherResponse);
         });
     });
