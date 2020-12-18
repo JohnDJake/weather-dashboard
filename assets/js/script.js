7f82db5b121e99753d079d739e2dfce6667ab4a4
JohnDJake <johndjake@gmail.com>
2020-12-17 19:35:03 -0800

    store and display search history, store last search and display it on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const DateTime = luxon.DateTime;
+var searchHistory;
 
 function updateWeather(city) {
     // Get the geographic coordinates from the OpenCage Geocoding API to pass to the OpenWeather Map onecall API
@@ -7,61 +8,98 @@ function updateWeather(city) {
         method: "GET"
     }).then(function (coordResponse) {
         const coordResult = coordResponse.results[0];
-        // Get the weather data using the coordinates from OpenCage
-        $.ajax({
-            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${coordResult.geometry.lat}&lon=${coordResult.geometry.lng}&exclude=minutely,hourly,alerts&units=imperial&appid=d299da3abaf6094099f1ec02d54a1339`,
-            method: "GET"
-        }).then(function (weatherResponse) {
-            const current = weatherResponse.current;
-            
-            // Set the title based off the location info from OpenCage and the date from OpenWeather
-            $("#title-today").text(`${coordResult.components.city}, ${coordResult.components.state_code},  ${coordResult.components["ISO_3166-1_alpha-3"]} (${DateTime.fromSeconds(current.dt).setZone(weatherResponse.timezone).toLocaleString()})`);
-
-            // Set the icon for the current weather and set title and alt text to the description
-            $("#icon-today").attr("src", `https://openweathermap.org/img/wn/${current.weather[0].icon}.png`);
-            $("#icon-today").attr("alt", current.weather[0].description);
-            $("#icon-today").attr("title", current.weather[0].description);
-
-            // Display today's weather
-            $("#temp-today").text(current.temp);
-            $("#humidity-today").text(current.humidity);
-            $("#wind-today").text(current.wind_speed);
-            $("#UV-today").text(current.uvi);
-            // UV badge color depends on the UV
-            if (current.uvi < 3) $("#UV-today").addClass("badge-success");
-            else if (current.uvi < 6) $("#UV-today").addClass("badge-warning");
-            else $("#UV-today").addClass("badge-danger");
-
-
-            // Display the forecast
-            // looping from 1 to five because those are the relevent indices in the weatherResponse.daily object and the numbers I used in my reference classes
-            for (var i = 1; i <= 5; i++) {
-                const forecast = weatherResponse.daily[i];
-
-                // Set the title to the date
-                $(`.card-title.day${i}`).text(DateTime.fromSeconds(forecast.dt).setZone(weatherResponse.timezone).toLocaleString());
-
-                // Display the weather icon and set title and alt text to the description
-                $(`.forecast-icon.day${i}`).attr("src", `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`);
-                $(`.forecast-icon.day${i}`).attr("alt", forecast.weather[0].description);
-                $(`.forecast-icon.day${i}`).attr("title", forecast.weather[0].description);
-
-                // Display the forecast info
-                $(`.forecast-temp.day${i}`).text(forecast.temp.max);
-                $(`.forecast-humidity.day${i}`).text(forecast.humidity);
+        // Make sure we found a valid city
+        // TODO use Bootstrap alert instead of vanilla JS alert
+        if (coordResult.components.city === undefined
+            || coordResult.components.state_code === undefined
+            || coordResult.components["ISO_3166-1_alpha-3"] === undefined) {
+            alert("We couldn't find a valid city from that search");
+        } else {
+            // City name formatted to display and store
+            const cityFullName = `${coordResult.components.city}, ${coordResult.components.state_code},  ${coordResult.components["ISO_3166-1_alpha-3"]}`;
+            // Store the city name
+            if (!searchHistory.includes(cityFullName)) {
+                searchHistory.push(cityFullName);
+                localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
+                localStorage.setItem("weatherSearchLast", cityFullName);
+                displayHistory();
             }
-        });
-    });
+            // Get the weather data using the coordinates from OpenCage
+            $.ajax({
+                url: `https://api.openweathermap.org/data/2.5/onecall?lat=${coordResult.geometry.lat}&lon=${coordResult.geometry.lng}&exclude=minutely,hourly,alerts&units=imperial&appid=d299da3abaf6094099f1ec02d54a1339`,
+                method: "GET"
+            }).then(function (weatherResponse) {
+                const current = weatherResponse.current;
 
+                // Set the title based off the location info from OpenCage and the date from OpenWeather
+                $("#title-today").text(`${cityFullName} (${DateTime.fromSeconds(current.dt).setZone(weatherResponse.timezone).toLocaleString()})`);
+
+                // Set the icon for the current weather and set title and alt text to the description
+                $("#icon-today").attr("src", `https://openweathermap.org/img/wn/${current.weather[0].icon}.png`);
+                $("#icon-today").attr("alt", current.weather[0].description);
+                $("#icon-today").attr("title", current.weather[0].description);
+
+                // Display today's weather
+                $("#temp-today").text(current.temp);
+                $("#humidity-today").text(current.humidity);
+                $("#wind-today").text(current.wind_speed);
+                $("#UV-today").text(current.uvi);
+                // UV badge color depends on the UV
+                if (current.uvi < 3) $("#UV-today").addClass("badge-success");
+                else if (current.uvi < 6) $("#UV-today").addClass("badge-warning");
+                else $("#UV-today").addClass("badge-danger");
+
+
+                // Display the forecast
+                // looping from 1 to five because those are the relevent indices in the weatherResponse.daily object and the numbers I used in my reference classes
+                for (var i = 1; i <= 5; i++) {
+                    const forecast = weatherResponse.daily[i];
+
+                    // Set the title to the date
+                    $(`.card-title.day${i}`).text(DateTime.fromSeconds(forecast.dt).setZone(weatherResponse.timezone).toLocaleString());
+
+                    // Display the weather icon and set title and alt text to the description
+                    $(`.forecast-icon.day${i}`).attr("src", `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`);
+                    $(`.forecast-icon.day${i}`).attr("alt", forecast.weather[0].description);
+                    $(`.forecast-icon.day${i}`).attr("title", forecast.weather[0].description);
+
+                    // Display the forecast info
+                    $(`.forecast-temp.day${i}`).text(forecast.temp.max);
+                    $(`.forecast-humidity.day${i}`).text(forecast.humidity);
+                }
+            });
+        }
+    });
 }
 
+function displayHistory() {
+    // Empty the list-group
+    $("#history-list").empty();
+    
+    // Loop through search history
+    for (var i = 0; i < searchHistory.length; i++) {
+        // Create button, add classes, set text, and append to list-group
+        $("<button>").addClass("list-group-item list-group-item-action history").text(searchHistory[i]).appendTo($("#history-list"));
+    }
+}
 
 $(document).ready(function () {
-    updateWeather("Seattle");
+    searchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory")) || [];
+    displayHistory();
 
-    $("#search-form").submit(function(event) {
+    $("#history-list").on("click", ".history", function (event) {
+        updateWeather($(this).text());
+    });
+
+    // If the user has used this weather app before, load their most recent search
+    var searchLast = localStorage.getItem("weatherSearchLast");
+    if (searchLast !== null) { updateWeather(searchLast); }
+
+    $("#search-form").submit(function (event) {
         event.preventDefault();
         updateWeather($("#city-search").val());
         $("#city-search").val("");
-    })
+    });
+
+    $(".alert").alert();
 })
